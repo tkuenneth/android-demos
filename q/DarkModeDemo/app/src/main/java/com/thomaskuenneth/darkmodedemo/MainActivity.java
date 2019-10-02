fe7503559e9f0c3d81210b8e4ba29fb79a0716bd
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2019-10-02 15:46:14 +0200

    DarkModeDemo added

---
+++ q/DarkModeDemo/app/src/main/java/com/thomaskuenneth/darkmodedemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.thomaskuenneth.darkmodedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

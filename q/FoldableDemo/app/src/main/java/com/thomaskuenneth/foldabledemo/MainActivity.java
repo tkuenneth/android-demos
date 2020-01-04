4c4beb909838810f85fb9ff0fe68675cec2e9c3c
Thomas Kuenneth <kuennetht@gmail.com>
2020-01-04 16:59:17 +0100

    FoldableDemo updated

--- q/FoldableDemo/app/src/main/java/com/thomaskuenneth/foldabledemo/MainActivity.java
+++ q/FoldableDemo/app/src/main/java/com/thomaskuenneth/foldabledemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,17 +22,32 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+    private TextView msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fl = findViewById(R.id.frameLayout);
+        msg = findViewById(R.id.msg);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        msg.setText("onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        msg.setText("onStop()");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        msg.setText("onResume()");
         handler = new Handler();
         handler.post(runnableCode);
     }
@@ -39,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        msg.setText("onPause()");
         handler = null;
     }
 

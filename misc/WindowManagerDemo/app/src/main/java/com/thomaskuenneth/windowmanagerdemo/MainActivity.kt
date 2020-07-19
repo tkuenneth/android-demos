8e45f206382843b113764e688d5e631b8182493c
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-07-19 16:58:11 +0200

    Moved from Log to ScrollView :-)

--- misc/WindowManagerDemo/app/src/main/java/com/thomaskuenneth/windowmanagerdemo/MainActivity.kt
+++ misc/WindowManagerDemo/app/src/main/java/com/thomaskuenneth/windowmanagerdemo/MainActivity.kt
@@ -1,38 +1,32 @@
 package com.thomaskuenneth.windowmanagerdemo
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.doOnLayout
 import androidx.window.DeviceState
 import androidx.window.ExtensionWindowBackend
 import androidx.window.WindowManager
-
-private val TAG = AppCompatActivity::class.simpleName
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
-
         val backend = ExtensionWindowBackend.getInstance(this)
         val wm = WindowManager(this, backend)
-
-        val posture = when (wm.deviceState.posture) {
-            DeviceState.POSTURE_CLOSED -> "CLOSED"
-            DeviceState.POSTURE_HALF_OPENED -> "HALF_OPENED"
-            DeviceState.POSTURE_OPENED -> "OPENED"
-            DeviceState.POSTURE_FLIPPED -> "FLIPPED"
-            // DeviceState.POSTURE_UNKNOWN
-            else -> "UNKNOWN"
-        }
-        Log.d(TAG, "Posture: $posture")
-
-        wm.windowLayoutInfo.displayFeatures.forEach {
-            Log.d(TAG, it.toString())
+        window.decorView.doOnLayout {
+            val posture = when (wm.deviceState.posture) {
+                DeviceState.POSTURE_CLOSED -> "CLOSED"
+                DeviceState.POSTURE_HALF_OPENED -> "HALF_OPENED"
+                DeviceState.POSTURE_OPENED -> "OPENED"
+                DeviceState.POSTURE_FLIPPED -> "FLIPPED"
+                // DeviceState.POSTURE_UNKNOWN
+                else -> "UNKNOWN"
+            }
+            textview.append("Posture: $posture")
+            wm.windowLayoutInfo.displayFeatures.forEach {
+                textview.append(it.toString())
+            }
         }
     }
 }

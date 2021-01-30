6bfff52bb209c09a892e7a5790b1d8d9953e4c35
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-01-30 15:21:08 +0100

    Project AliasActivityDemo added

---
+++ misc/AliasActivityDemo/app/src/main/java/com/thomaskuenneth/aliasactivitydemo/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.thomaskuenneth.aliasactivitydemo
+
+import android.app.AliasActivity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AliasActivity() {
+}
---
+++ misc/AliasActivityDemo/app/src/main/java/com/thomaskuenneth/aliasactivitydemo/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.thomaskuenneth.aliasactivitydemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

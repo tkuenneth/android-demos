4af1c051c8c3f26e4c73a31042e3685260a97ceb
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2021-02-18 17:02:49 +0100

    Beautified code

--- misc/RoleDemo/app/src/main/java/com/thomaskuenneth/roledemo/MainActivity.kt
+++ misc/RoleDemo/app/src/main/java/com/thomaskuenneth/roledemo/MainActivity.kt
@@ -15,17 +15,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         getSystemService(RoleManager::class.java)?.run {
-            if (!isRoleHeld(RoleManagerCompat.ROLE_BROWSER)) {
-                val intent = createRequestRoleIntent(RoleManagerCompat.ROLE_BROWSER)
-                startActivityForResult(intent, REQUEST_ROLE_BROWSER)
-            } else Toast.makeText(baseContext, R.string.has_role, Toast.LENGTH_SHORT).show()
+            if (isRoleAvailable(RoleManagerCompat.ROLE_BROWSER)) {
+                if (!isRoleHeld(RoleManagerCompat.ROLE_BROWSER)) {
+                    val intent = createRequestRoleIntent(RoleManagerCompat.ROLE_BROWSER)
+                    startActivityForResult(intent, REQUEST_ROLE_BROWSER)
+                } else
+                    toast(R.string.has_role)
+            } else toast(R.string.role_not_available)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_ROLE_BROWSER && resultCode == RESULT_OK) {
-            println("ok")
+            toast(R.string.role_acquired)
         }
+    }
+
+    private fun toast(resId: Int) {
+        Toast.makeText(baseContext, resId, Toast.LENGTH_SHORT).show()
     }
 }

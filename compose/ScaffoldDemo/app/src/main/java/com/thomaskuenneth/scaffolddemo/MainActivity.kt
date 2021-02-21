fbedd6611c9dbc98abf7748c56fc6515df9ccb53
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-02-21 13:25:19 +0100

    DropDownMenu added

--- compose/ScaffoldDemo/app/src/main/java/com/thomaskuenneth/scaffolddemo/MainActivity.kt
+++ compose/ScaffoldDemo/app/src/main/java/com/thomaskuenneth/scaffolddemo/MainActivity.kt
@@ -3,15 +3,17 @@ package com.thomaskuenneth.scaffolddemo
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons.Default
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import com.thomaskuenneth.scaffolddemo.ui.theme.ScaffoldDemoTheme
-
+import androidx.compose.runtime.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Content() {
+    var menuOpened by remember { mutableStateOf(false) }
     ScaffoldDemoTheme {
         Scaffold(topBar = {
             TopAppBar(title = {
@@ -33,15 +36,38 @@ fun Content() {
                 }
             },
                 actions = {
-                    IconButton(onClick = {
-                        println("Icons.Default.Add")
-                    }) {
-                        Icon(Default.Add, null)
-                    }
-                    IconButton(onClick = {
-                        println("Icons.Default.Delete")
-                    }) {
-                        Icon(Default.Delete, null)
+//                    IconButton(onClick = {
+//                        println("Icons.Default.Add")
+//                    }) {
+//                        Icon(Default.Add, null)
+//                    }
+//                    IconButton(onClick = {
+//                        println("Icons.Default.Delete")
+//                    }) {
+//                        Icon(Default.Delete, null)
+//                    }
+                    Box {
+                        IconButton(onClick = {
+                            menuOpened = true
+                        }) {
+                            Icon(Default.MoreVert, null)
+                            DropdownMenu(expanded = menuOpened,
+                                onDismissRequest = {
+                                    menuOpened = false
+                                }) {
+                                DropdownMenuItem(onClick = {
+                                    menuOpened = false
+                                }) {
+                                    Text("Item #1")
+                                }
+                                Divider()
+                                DropdownMenuItem(onClick = {
+                                    menuOpened = false
+                                }) {
+                                    Text("Item #2")
+                                }
+                            }
+                        }
                     }
                 })
         }) {

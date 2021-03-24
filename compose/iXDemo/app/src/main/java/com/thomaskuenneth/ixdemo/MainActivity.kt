378e4a05e4c1e00d8d824a89df8fd3fe61819c86
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2021-03-24 15:03:27 +0100

    work in progress

--- compose/iXDemo/app/src/main/java/com/thomaskuenneth/ixdemo/MainActivity.kt
+++ compose/iXDemo/app/src/main/java/com/thomaskuenneth/ixdemo/MainActivity.kt
@@ -3,22 +3,73 @@ package com.thomaskuenneth.ixdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             // Greeting()
-            LayoutDemo()
+            // LayoutDemo()
+            // ButtonDemo()
+            TextFieldDemo()
         }
+    }
+}
+
+@Composable
+fun TextFieldDemo() {
+    var input by remember { mutableStateOf(TextFieldValue()) }
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = input,
+            onValueChange = {
+                input = it
+            }
+        )
+        Button(
+            onClick = {
+                println(input.text)
+            },
+            enabled = input.text.isNotEmpty()
+        ) {
+            Text(stringResource(id = R.string.click))
+        }
+    }
+}
+
+@Composable
+fun ButtonDemo() {
+    Button(onClick = {
+        println("clicked")
+    }) {
+//                Column {
+//                    Text("Zeile 1")
+//                    Text("Zeile 2")
+//                }
+        Text(stringResource(id = R.string.click))
     }
 }
 
@@ -45,7 +96,17 @@ fun LayoutDemo() {
     }
 }
 
+fun Modifier.drawOnYellow() = this.drawBehind {
+    drawRect(color = Color.Yellow)
+}
+
 @Composable
 fun Greeting() {
-    Text(text = "Hello iX")
+    Text(text = "Hello iX",
+        modifier = Modifier
+            .clickable { println("Hello iX") }
+            .border(
+                1.dp,
+                MaterialTheme.colors.primary
+            ))
 }

3fb394eb40dca36915548cb57e0e91e5237431a0
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-09-08 16:35:06 +0200

    Minor updates

--- misc/JetpackSplashScreenDemo/app/src/main/java/com/thomaskuenneth/jetpacksplashscreendemo/MainActivity.kt
+++ misc/JetpackSplashScreenDemo/app/src/main/java/com/thomaskuenneth/jetpacksplashscreendemo/MainActivity.kt
@@ -2,12 +2,19 @@ package com.thomaskuenneth.jetpacksplashscreendemo
 
 import android.animation.AnimatorSet
 import android.animation.ValueAnimator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,15 +30,34 @@ class MainActivity : AppCompatActivity() {
                     icon.layoutParams.height = value
                     icon.requestLayout()
                     if (value == 0) {
-                        setContentView(R.layout.activity_main)
+                        view.remove()
                     }
                 }
                 val animationSet = AnimatorSet()
                 animationSet.interpolator = AccelerateDecelerateInterpolator()
-                animationSet.play(animator);
+                animationSet.play(animator)
                 animationSet.start()
             }
         }
 
+        var ready = false
+        setContentView(R.layout.activity_main)
+        val content = findViewById<View>(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (ready) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        )
+        lifecycleScope.launch(Dispatchers.IO) {
+            delay(5000)
+            ready = true
+        }
     }
 }

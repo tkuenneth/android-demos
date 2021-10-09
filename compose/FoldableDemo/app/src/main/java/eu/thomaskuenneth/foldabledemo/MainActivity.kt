81cb04faa6319d0723e1660561cb06962c90e35a
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-10-09 10:24:15 +0200

    Lib update

--- compose/FoldableDemo/app/src/main/java/eu/thomaskuenneth/foldabledemo/MainActivity.kt
+++ compose/FoldableDemo/app/src/main/java/eu/thomaskuenneth/foldabledemo/MainActivity.kt
@@ -9,35 +9,46 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoRepository.Companion.windowInfoRepository
+import androidx.window.layout.WindowLayoutInfo
+import androidx.window.layout.WindowMetrics
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val repo = windowInfoRepository()
         super.onCreate(savedInstanceState)
         setContent {
-            FoldableDemo()
+            val metrics by repo.currentWindowMetrics.collectAsState(null)
+            val info by repo.windowLayoutInfo.collectAsState(null)
+            Scaffold(modifier = Modifier.fillMaxSize(),
+                topBar = {
+                    TopAppBar(title = {
+                        Text(stringResource(id = R.string.app_name))
+                    })
+                }) {
+                Content(
+                    metrics,
+                    info
+                )
+            }
         }
     }
 }
 
-@Preview
 @Composable
-fun FoldableDemo() {
-    Scaffold(modifier = Modifier.fillMaxSize(),
-        topBar = {
-            TopAppBar(title = {
-                Text(stringResource(id = R.string.app_name))
-            })
-        }) {
-        Content()
+fun Content(
+    windowMetrics: WindowMetrics?,
+    windowLayoutInfo: WindowLayoutInfo?
+) {
+    windowLayoutInfo?.displayFeatures?.forEach { displayFeature ->
+        (displayFeature as FoldingFeature).run {
+        }
     }
-}
-
-@Preview
-@Composable
-fun Content() {
     Surface(modifier = Modifier.fillMaxSize()) {
 
     }

2545d23b45dab06e3c6365595fec81a2c45f9377
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-10-09 12:24:25 +0200

    added some arithmetics :-)

--- compose/FoldableDemo/app/src/main/java/eu/thomaskuenneth/foldabledemo/MainActivity.kt
+++ compose/FoldableDemo/app/src/main/java/eu/thomaskuenneth/foldabledemo/MainActivity.kt
@@ -3,7 +3,9 @@ package eu.thomaskuenneth.foldabledemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,7 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.window.layout.FoldingFeature
 import androidx.window.layout.WindowInfoRepository.Companion.windowInfoRepository
 import androidx.window.layout.WindowLayoutInfo
@@ -45,11 +50,66 @@ fun Content(
     windowMetrics: WindowMetrics?,
     windowLayoutInfo: WindowLayoutInfo?
 ) {
-    windowLayoutInfo?.displayFeatures?.forEach { displayFeature ->
-        (displayFeature as FoldingFeature).run {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        windowMetrics?.let { wm ->
+            val widthDp = with(LocalDensity.current) {
+                wm.bounds.width().toDp()
+            }
+            if (widthDp < 600.dp) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Yellow)
+                )
+            } else {
+                var hasGap = false
+                var sizeLeft = 0
+                var sizeRight = 0
+                var widthGap = 0
+                windowLayoutInfo?.displayFeatures?.forEach { displayFeature ->
+                    (displayFeature as FoldingFeature).run {
+                        hasGap = occlusionType == FoldingFeature.OcclusionType.FULL
+                                && orientation == FoldingFeature.Orientation.VERTICAL
+                        sizeLeft = bounds.left
+                        sizeRight = wm.bounds.width() - bounds.right
+                        widthGap = bounds.width()
+                    }
+                }
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    if (hasGap) {
+                        val width = (wm.bounds.width() - widthGap).toFloat()
+                        val weightLeft = sizeLeft.toFloat() / width
+                        val weightRight = sizeRight.toFloat() / width
+                        ColouredBoxWithWeight(weightLeft, Color.Red)
+                        Spacer(
+                            modifier = Modifier
+                                .requiredWidth(with(LocalDensity.current) {
+                                    widthGap.toDp()
+                                })
+                                .fillMaxHeight()
+                        )
+                        ColouredBoxWithWeight(weightRight, Color.Green)
+                    } else {
+                        ColouredBoxWithWeight(0.333F, Color.Red)
+                        ColouredBoxWithWeight(0.333F, Color.Green)
+                        ColouredBoxWithWeight(0.333F, Color.Blue)
+                    }
+                    val weightLeft = if (hasGap) 1F else 0.5F
+                }
+            }
         }
     }
-    Surface(modifier = Modifier.fillMaxSize()) {
+}
 
-    }
+@Composable
+fun RowScope.ColouredBoxWithWeight(weight: Float, color: Color) {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .weight(weight)
+            .background(color)
+            .border(1.dp, Color.White)
+    )
 }

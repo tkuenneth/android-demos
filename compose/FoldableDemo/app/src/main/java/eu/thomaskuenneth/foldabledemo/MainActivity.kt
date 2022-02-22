dc4f19efe19306e2d9c66c84e5001702f3983a01
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2022-02-22 14:59:42 +0100

    Refactoring

--- compose/FoldableDemo/app/src/main/java/eu/thomaskuenneth/foldabledemo/MainActivity.kt
+++ compose/FoldableDemo/app/src/main/java/eu/thomaskuenneth/foldabledemo/MainActivity.kt
@@ -18,28 +18,37 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.window.layout.FoldingFeature
-import androidx.window.layout.WindowInfoRepository.Companion.windowInfoRepository
-import androidx.window.layout.WindowLayoutInfo
-import androidx.window.layout.WindowMetrics
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.window.layout.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val r = windowInfoRepository()
         super.onCreate(savedInstanceState)
-        setContent {
-            val m by r.currentWindowMetrics.collectAsState(null)
-            val i by r.windowLayoutInfo.collectAsState(null)
-            Scaffold(modifier = Modifier.fillMaxSize(),
-                topBar = {
-                    TopAppBar(title = {
-                        Text(stringResource(id = R.string.app_name))
-                    })
-                }) {
-                Content(
-                    m,
-                    i
-                )
+        lifecycleScope.launch(Dispatchers.Main) {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                setContent {
+                    val metrics = WindowMetricsCalculator.getOrCreate()
+                        .computeCurrentWindowMetrics(this@MainActivity)
+                    val layoutInfo by WindowInfoTracker.getOrCreate(this@MainActivity)
+                        .windowLayoutInfo(this@MainActivity).collectAsState(
+                            initial = null
+                        )
+                    Scaffold(modifier = Modifier.fillMaxSize(),
+                        topBar = {
+                            TopAppBar(title = {
+                                Text(stringResource(id = R.string.app_name))
+                            })
+                        }) {
+                        Content(
+                            metrics,
+                            layoutInfo
+                        )
+                    }
+                }
             }
         }
     }

85c145b2e6e70279bcdffcc521aa096c15853cd3
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2022-02-22 12:35:24 +0100

    Library and tools upgrades

--- misc/WindowManagerDemo2/app/src/main/java/com/thomaskuenneth/windowmanagerdemo/MainActivity.kt
+++ misc/WindowManagerDemo2/app/src/main/java/com/thomaskuenneth/windowmanagerdemo/MainActivity.kt
@@ -2,9 +2,12 @@ package com.thomaskuenneth.windowmanagerdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.window.layout.FoldingFeature
-import androidx.window.layout.WindowInfoRepository.Companion.windowInfoRepository
+import androidx.window.layout.WindowInfoTracker
+import androidx.window.layout.WindowMetricsCalculator
 import com.thomaskuenneth.windowmanagerdemo.databinding.MainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
@@ -18,25 +21,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val repo = windowInfoRepository()
-        lifecycleScope.launch {
-            repo.currentWindowMetrics.collect { windowMetrics ->
-                output("Bounds: ${windowMetrics.bounds.toShortString()}")
-            }
-        }
-        lifecycleScope.launch {
-            repo.windowLayoutInfo.collect {
-                it.displayFeatures.forEach { displayFeature ->
-                    (displayFeature as FoldingFeature).run {
-                        output("occlusionType: $occlusionType")
-                        output("orientation: $orientation")
-                        output("state: $state")
-                        output("isSeparating: $isSeparating")
+        lifecycleScope.launch(Dispatchers.Main) {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                output("computeCurrentWindowMetrics():")
+                output(
+                    WindowMetricsCalculator.getOrCreate()
+                        .computeCurrentWindowMetrics(this@MainActivity).bounds.toString()
+                )
+                output("\nwindowLayoutInfo:")
+                WindowInfoTracker.getOrCreate(this@MainActivity)
+                    .windowLayoutInfo(this@MainActivity).collect {
+                        it.displayFeatures.forEach { displayFeature ->
+                            (displayFeature as FoldingFeature).run {
+                                output("occlusionType: $occlusionType")
+                                output("orientation: $orientation")
+                                output("state: $state")
+                                output("isSeparating: $isSeparating")
+                                output(this.bounds.toString())
+                            }
+                        }
                     }
-                }
             }
         }
     }
+
 
     private fun output(s: String) {
         lifecycleScope.launch(Dispatchers.Main) {

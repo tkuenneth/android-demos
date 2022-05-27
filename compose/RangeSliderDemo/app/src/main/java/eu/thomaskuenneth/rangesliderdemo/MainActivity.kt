2aefa541f130e4d2820a66847b829f874dbfc664
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-05-27 13:00:39 +0200

    Project RangeSliderDemo added

---
+++ compose/RangeSliderDemo/app/src/main/java/eu/thomaskuenneth/rangesliderdemo/MainActivity.kt
@@ -0,0 +1,22 @@
+package eu.thomaskuenneth.rangesliderdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+            }
+        }
+    }
+}

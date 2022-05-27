08adaeadd7b90dc534cc00bc663ead483b23c095
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-05-27 13:15:02 +0200

    Slider added

--- compose/RangeSliderDemo/app/src/main/java/eu/thomaskuenneth/rangesliderdemo/MainActivity.kt
+++ compose/RangeSliderDemo/app/src/main/java/eu/thomaskuenneth/rangesliderdemo/MainActivity.kt
@@ -3,11 +3,22 @@ package eu.thomaskuenneth.rangesliderdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +27,22 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    var values by remember { mutableStateOf((14.0F..16.0F)) }
+                    RangeSlider(
+                        values = values,
+                        onValueChange = {
+                            values = it
+                        },
+                        valueRange = (10.0F..20.0F),
+                        steps = 10
+                    )
+                }
             }
         }
     }

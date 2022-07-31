b1d647acb41e80b0cf6a22ad2c85e16ec2789035
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-07-31 11:45:43 +0200

    Compose version finished

--- compose/MarginPaddingDemo/app/src/main/java/eu/thomaskuenneth/compose/marginpaddingdemo/MarginPaddingDemoctivity.kt
+++ compose/MarginPaddingDemo/app/src/main/java/eu/thomaskuenneth/compose/marginpaddingdemo/MarginPaddingDemoctivity.kt
@@ -6,16 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import eu.thomaskuenneth.compose.marginpaddingdemo.ui.theme.MarginPaddingDemoTheme
+import eu.thomaskuenneth.compose.marginpaddingdemo.ui.theme.Purple200
+import eu.thomaskuenneth.compose.marginpaddingdemo.ui.theme.Purple500
+import eu.thomaskuenneth.compose.marginpaddingdemo.ui.theme.Teal700
 
 class MarginPaddingDemoctivity : ComponentActivity() {
     @SuppressLint("InflateParams")
@@ -46,12 +53,47 @@ fun MainScreen(layout: View) {
                     }
                 )
                 Box(
+                    contentAlignment = Alignment.Center,
                     modifier = Modifier
                         .fillMaxWidth()
                         .weight(0.5f)
                 ) {
-
+                    Buttons()
                 }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun Buttons() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(color = Teal700)
+            .padding(32.dp)
+    ) {
+        OutlinedButton(modifier = Modifier
+            .background(Purple200)
+            .padding(32.dp),
+            onClick = {}) {
+            Text("Button #1")
+        }
+        Box(
+            modifier = Modifier
+                .background(color = Purple500)
+                .padding(32.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            OutlinedButton(
+                onClick = {}) {
+                Text(
+                    text = "Button #2",
+                    modifier = Modifier.padding(
+                        32.dp
+                    )
+                )
             }
         }
     }

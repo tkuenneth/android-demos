9b4795ed9200f7429a0b844495fce9b1c346ab7a
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-08-24 17:38:22 +0200

    Added support for App languages

--- t/PhotoPickerDemo/app/src/main/java/eu/thomaskuenneth/photopickerdemo/PhotoPickerDemoActivity.kt
+++ t/PhotoPickerDemo/app/src/main/java/eu/thomaskuenneth/photopickerdemo/PhotoPickerDemoActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text

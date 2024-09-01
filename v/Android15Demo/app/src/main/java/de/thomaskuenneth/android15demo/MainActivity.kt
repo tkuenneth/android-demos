aa532f23f247f6ffe78e3b364f175c9f39c10956
Thomas Künneth <kuennetht@gmail.com>
2024-09-01 15:52:53 +0200

    Project Android15Demo added

---
+++ v/Android15Demo/app/src/main/java/de/thomaskuenneth/android15demo/MainActivity.kt
@@ -0,0 +1,35 @@
+package de.thomaskuenneth.android15demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .background(color = MaterialTheme.colorScheme.secondaryContainer)
+            .fillMaxSize()
+    )
+}

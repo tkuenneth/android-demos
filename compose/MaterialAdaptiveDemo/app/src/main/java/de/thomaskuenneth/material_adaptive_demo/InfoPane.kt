816af6c8094359482cd31ef9a84cb0b1e3d01531
Thomas Künneth <kuennetht@gmail.com>
2024-09-09 12:09:11 +0200

    InfoPane almost finished

--- compose/MaterialAdaptiveDemo/app/src/main/java/de/thomaskuenneth/material_adaptive_demo/InfoPane.kt
+++ compose/MaterialAdaptiveDemo/app/src/main/java/de/thomaskuenneth/material_adaptive_demo/InfoPane.kt
@@ -1,8 +1,92 @@
 package de.thomaskuenneth.material_adaptive_demo
 
+import androidx.activity.compose.BackHandler
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.PaneAdaptedValue
+import androidx.compose.material3.adaptive.layout.SupportingPaneScaffold
+import androidx.compose.material3.adaptive.layout.SupportingPaneScaffoldRole
+import androidx.compose.material3.adaptive.layout.ThreePaneScaffoldScope
+import androidx.compose.material3.adaptive.navigation.ThreePaneScaffoldNavigator
+import androidx.compose.material3.adaptive.navigation.rememberSupportingPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
 @Composable
 fun InfoPane() {
+    val navigator = rememberSupportingPaneScaffoldNavigator()
+    BackHandler(navigator.canNavigateBack()) {
+        navigator.navigateBack()
+    }
+    SupportingPaneScaffold(
+        directive = navigator.scaffoldDirective,
+        mainPane = { MainPane(navigator = navigator) },
+        supportingPane = { SupportingPane(navigator = navigator) },
+        value = navigator.scaffoldValue
+    )
+}
 
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@Composable
+fun ThreePaneScaffoldScope.MainPane(navigator: ThreePaneScaffoldNavigator<Nothing>) {
+    ColoredBox(color = MaterialTheme.colorScheme.primaryContainer,
+        resIdMessage = R.string.main_pane,
+        resIdButton = R.string.show_supporting_pane,
+        shouldShowButton = navigator.scaffoldValue[SupportingPaneScaffoldRole.Supporting] == PaneAdaptedValue.Hidden,
+        onClick = { navigator.navigateTo(SupportingPaneScaffoldRole.Supporting) })
+}
+
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@Composable
+fun ThreePaneScaffoldScope.SupportingPane(navigator: ThreePaneScaffoldNavigator<Nothing>) {
+    ColoredBox(color = MaterialTheme.colorScheme.secondaryContainer,
+        resIdMessage = R.string.supporting_pane,
+        resIdButton = R.string.show_main_pane,
+        shouldShowButton = navigator.scaffoldValue[SupportingPaneScaffoldRole.Main] == PaneAdaptedValue.Hidden,
+        onClick = { navigator.navigateTo(SupportingPaneScaffoldRole.Main) })
+}
+
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@Composable
+private fun ThreePaneScaffoldScope.ColoredBox(
+    color: Color,
+    shouldShowButton: Boolean,
+    @StringRes resIdMessage: Int,
+    @StringRes resIdButton: Int,
+    onClick: () -> Unit
+) {
+    AnimatedPane {
+        Box(
+            modifier = Modifier.background(color), contentAlignment = Alignment.BottomEnd
+        ) {
+            Text(
+                text = stringResource(resIdMessage),
+                style = MaterialTheme.typography.displayLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(
+                    Alignment.Center
+                )
+            )
+            if (shouldShowButton) {
+                Button(onClick = onClick, modifier = Modifier.padding(all = 32.dp)) {
+                    Text(
+                        text = stringResource(resIdButton)
+                    )
+                }
+            }
+        }
+    }
 }

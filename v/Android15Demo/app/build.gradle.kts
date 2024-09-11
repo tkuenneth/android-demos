ca8577affc552bd42098db3fc37682a34c31ff52
Thomas Künneth <kuennetht@gmail.com>
2024-09-11 11:47:50 +0200

    How to use windowOptOutEdgeToEdgeEnforcement

--- v/Android15Demo/app/build.gradle.kts
+++ v/Android15Demo/app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
